7efccbd34666cc2bd9dee991dccfaad9be241329
Mendel Cohen <mmhakohen@gmail.com>
2021-12-12 22:37:31 -0500

    CRUD commands completed: completes all CRUD commands enabling calls to the database

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -1,27 +1,42 @@
 class Api::CoursesController < ApplicationController
 
   def index
-    @courses = "All Courses"
+    @courses = Course.all
     render "index.json.jb"
   end
 
   def create
-    @course = "Create Course"
-    render = "show.json.jb"
+    @course = Course.new(
+      name: params[:name],
+      release_date: params[:release_date]
+    )
+    if @course.save
+      render "show.json.jb"
+    else
+      render json: { errors: @course.errors.full_messages}, status: :bad_request
+    end
   end
 
   def show
-    @course = "Show Course"
-    render = "show.json.jb"
-  end
-
-  def update
-    @course = "Update Course"
+    @course = Course.find_by(id: params[:id])
     render "show.json.jb"
   end
 
+  def update
+    @course = Course.find_by(id: params[:id])
+    @course.name = params[:name] || @course.name
+    @course.release_date = params[:release_date] || @course.release_date
+    render "show.json.jb"
+    if @course.save
+      render "show.json.jb"
+    else
+      render json: { errors: @course.errors.full_messages}, status: :bad_request
+    end
+  end
+
   def destroy
-    @course = "Delete Course"
+    course = Course.find_by(id: params[:id])
+    course.destroy
     render json: { message: "COURSE DELETED"}
   end
 
